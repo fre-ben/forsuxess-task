933717001e6c605666c93d54580d289543d9772b
fre-ben <fschocke@gmail.com>
2021-05-08 18:14:43 +0200

    🚧 ✨ Add writeJobsInDB() function Parses a XML file and writes formatted data into a DB For testing it only writes one job right now

--- server/db.ts
+++ server/db.ts
@@ -1,4 +1,6 @@
 import { Db, MongoClient } from "mongodb";
+import { cleanTitle, parseXml } from "../utils/helpers";
+import { readXml } from "./xmlReader";
 
 let client: MongoClient = null;
 let db: Db = null;
@@ -17,10 +19,37 @@ export async function connectDB(url: string, dbName: string) {
   db = client.db(dbName);
 }
 
-export async function getJobs() {
+export async function getJobs(): Promise<Job[]> {
   await connectDB(url, dbName);
 
-  const jobs = await db.collection("jobs").find({}).toArray();
+  const jobs: Job[] = await db.collection("jobs").find({}).toArray();
 
   return jobs;
+}
+
+export async function writeJobsInDB() {
+  await connectDB(url, dbName);
+  const jobsCollection = db.collection("jobs");
+
+  const xmlFile = await readXml("jobdata_1");
+  const parsedXml = await parseXml(xmlFile);
+
+  const singleJob = parsedXml.jobList.job[4];
+  // Beispielhaft ersten Eintrag lesen und entsprechend des Job Types einpflegen in DB
+  const newJob: Job = {
+    id: +singleJob.$.refno,
+    title: cleanTitle(
+      singleJob.languageSpecificElements[0].languageSpecificElement[0]
+        .posTitle[0]._
+    ),
+    company: singleJob.Organization[0],
+  };
+
+  if (await jobsCollection.findOne({ id: newJob.id })) {
+    console.log("Job already exists");
+    return;
+  } else {
+    console.log("Job posted");
+    return jobsCollection.insertOne(newJob);
+  }
 }
