2d55461c8c4bb48e0ad5311f2323a94e9c1774b9
fre-ben <fschocke@gmail.com>
2021-05-08 18:15:20 +0200

    ✨ Add helper function, which cleans posTitle data from XML

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -5,3 +5,7 @@ const parser = new xml2js.Parser();
 export async function parseXml(xmlFile: Buffer) {
   return await parser.parseStringPromise(xmlFile);
 }
+
+export function cleanTitle(oldTitle: string): string {
+  return oldTitle.replace(/\r\n\s/g, "").replace(/\s\r\n/g, "");
+}
